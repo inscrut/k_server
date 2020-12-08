f4cc5c705747514fe28f8bf93e4e38a276323541
Елена Романова <elena.romanova.1999@mail.ru>
2020-12-08 23:17:15 +0700

    Add bdconnector class; change database;

---
+++ K_Server/BDConnector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace K_Server
+{
+    static class BDConnector
+    {
+        public static void InitDB()
+        {
+            
+            string ConnectionString = @"Data Source=.\SQLEXPRESS;
+                                      AttachDbFilename=c:\folder\SampleDatabase.mdf;
+                                      Integrated Security=True;
+                                      Connect Timeout=30;
+                                      User Instance=True";
+
+        }
+        public static void CreateCommand(string queryString,
+        string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(
+                       connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
