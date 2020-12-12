f25d7a0734f09baa8589ee02848d8bdf470c28a2
Елена Романова <elena.romanova.1999@mail.ru>
2020-12-12 18:27:04 +0700

    Modify DB init with checks; add getlastmsg for grp

--- K_Server/BDConnector.cs
+++ K_Server/BDConnector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Microsoft.Data.Sqlite;
 
@@ -11,10 +12,34 @@ namespace K_Server
 
         public static void InitDB()
         {
+            if (File.Exists("General.db"))
+            {
+                Console.WriteLine("DB: General.db was finded");
+            }
+            else
+            {
+                Console.WriteLine("DB: (ERR) file General.db not found, exit");
+                Environment.Exit(1);
+            }
+
+            try
+            {
+                using (var fs = File.Open("General.db", FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    Console.WriteLine("DB: file is free");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("DB: file is busy, exit");
+                Environment.Exit(1);
+            }
+
+
             connection = new SqliteConnection("Data Source=General.db");
-            Console.WriteLine("Connect to DB");
+            Console.WriteLine("DB: Connected");
         }
-        public static string[] CreateCommand(string queryString)
+        public static string[] CreateCommand(string queryString, int _clmn)
         {
             List<string> ans = new List<string>();
 
@@ -38,7 +63,7 @@ namespace K_Server
                 {
                     while (reader.Read())
                     {
-                        var st = reader.GetString(0);
+                        var st = reader.GetString(_clmn);
                         ans.Add(st);
                     }
                 }
@@ -56,7 +81,7 @@ namespace K_Server
 
         public static string getFacult(string _group)
         {
-            var r = CreateCommand("SELECT facults FROM usergroups WHERE groupname = \""+ _group  + "\"");
+            var r = CreateCommand("SELECT facults FROM usergroups WHERE groupname = \""+ _group  + "\"", 0);
             if (r == null) return null;
             if (r.Length <= 0) return null;
             return r[0];
@@ -64,7 +89,7 @@ namespace K_Server
 
         public static string getPasswd(string _user)
         {
-            var r = CreateCommand("SELECT passwd FROM users WHERE username = \"" + _user + "\"");
+            var r = CreateCommand("SELECT passwd FROM users WHERE username = \"" + _user + "\"", 0);
             if (r == null) return null;
             if (r.Length <= 0) return null;
             return r[0];
@@ -72,7 +97,7 @@ namespace K_Server
 
         public static string getGroup(string _user)
         {
-            var r = CreateCommand("SELECT ugroup FROM users WHERE username = \"" + _user + "\"");
+            var r = CreateCommand("SELECT ugroup FROM users WHERE username = \"" + _user + "\"", 0);
             if (r == null) return null;
             if (r.Length <= 0) return null;
             return r[0];
@@ -80,10 +105,54 @@ namespace K_Server
 
         public static string[] getChatsFlow(string _flow)
         {
-            var r = CreateCommand("SELECT groupname FROM usergroups WHERE facults = \"" + _flow + "\"");
+            var r = CreateCommand("SELECT groupname FROM usergroups WHERE facults = \"" + _flow + "\"", 0);
             if (r == null) return null;
             if (r.Length <= 0) return null;
             return r;
+        }
+
+        public static string[] getLastMsgGrp(string _grp)
+        {
+            List<string> ans = new List<string>();
+
+            try
+            {
+                connection.Open();
+            }
+            catch (SqliteException e)
+            {
+                Console.WriteLine("ERR: (open db) " + e.Message);
+                return null;
+            }
+
+            var command = connection.CreateCommand();
+
+            command.CommandText = "SELECT id,ugroup,username,time,message FROM chats WHERE ugroup = \"" + _grp + "\" ORDER BY id ASC LIMIT 10;";
+
+            try
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var st = reader.GetString(0) + ","
+                            + reader.GetString(1) + ","
+                            + reader.GetString(2) + ","
+                            + reader.GetString(3) + ","
+                            + reader.GetString(4);
+                        ans.Add(st);
+                    }
+                }
+
+                connection.Close();
+            }
+            catch (SqliteException e)
+            {
+                Console.WriteLine("ERR: (exec db) " + e.Message);
+                return null;
+            }
+
+            return ans.ToArray();
         }
     }
 }
